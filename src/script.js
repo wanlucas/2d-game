53a7bc6ac9ad33c2a1146f7bf73d408340415f00
Wan Lucas <wan.lucas.90@gmail.com>
2022-10-23 17:34:05 -0300

    :sparkles: adding map generation system

--- src/script.js
+++ src/script.js
@@ -1,4 +1,92 @@
+import maps from './maps/maps.js';
 import Player from './class/Player.js';
+import Platform from './class/Platform.js';
+import {
+  ctx,
+  canvasW,
+  canvasH,
+  inputs,
+  platforms,
+  config,
+  players,
+} from './game.js';
 
-const canvas = document.getElementById('canvas');
-const { width, height } = canvas;
+const setInput = (input, value) => {
+  switch (input) {
+    case 'KeyD':
+      inputs.right = value;
+      break;
+
+    case 'KeyA':
+      inputs.left = value;
+      break;
+
+    case 'Space':
+      inputs.jump = value;
+      break;
+    default:
+  }
+};
+
+const createPlatform = (i, j, type) => {
+  const { tileSize } = config;
+  let width;
+  let height;
+
+  switch (type) {
+    case '_':
+      width = 50;
+      height = 20;
+      break;
+    default:
+      width = 50;
+      height = 50;
+  }
+
+  platforms.push(
+    new Platform(
+      i * tileSize,
+      j * tileSize,
+      width,
+      height,
+    ),
+  );
+};
+
+const createPlayer = (x, y, weight) => {
+  players.push(
+    new Player(x, y, weight),
+  );
+};
+
+const createMap = (map) => {
+  maps[map].forEach((line, j) => {
+    line.forEach((type, i) => {
+      if (/[_+]/.test(type)) createPlatform(i, j, type);
+    });
+  });
+};
+
+function render() {
+  ctx.clearRect(0, 0, canvasW, canvasH);
+
+  players.forEach((player) => {
+    player.update();
+  });
+
+  platforms.forEach((platform) => {
+    platform.update();
+  });
+
+  requestAnimationFrame(render);
+}
+
+window.onload = () => {
+  createMap(config.map);
+  createPlayer(100, canvasH - 50, 50);
+  render();
+};
+
+window.onkeydown = ({ code }) => setInput(code, true);
+
+window.onkeyup = ({ code }) => setInput(code, false);
