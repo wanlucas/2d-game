4354689af872d899df499480d7294e9fb3e62a96
Wan Lucas <wan.lucas.90@gmail.com>
2022-10-23 17:43:21 -0300

    :recycle: updating collision system

--- src/functions/collision.js
+++ src/functions/collision.js
@@ -1,13 +1,13 @@
 export const rectanglesHorizontalCollision = (r1, r2) => (
   r1.position.x + r1.width + r1.velocity.x >= r2.position.x
   && r1.position.x + r1.velocity.x <= r2.position.x + r2.width
-  && r1.position.y + r1.height >= r2.position.y
-  && r1.position.y <= r2.position.y + r2.height
+  && r1.position.y + r1.height > r2.position.y
+  && r1.position.y < r2.position.y + r2.height
 );
 
 export const rectanglesVerticalCollision = (r1, r2) => (
   r1.position.x + r1.width >= r2.position.x
   && r1.position.x <= r2.position.x + r2.width
-  && r1.position.y + r1.velocity.y + r1.height >= r2.position.y
-  && r1.position.y + r1.velocity.y <= r2.position.y + r2.height
+  && r1.position.y + r1.velocity.y + r1.height > r2.position.y
+  && r1.position.y + r1.velocity.y < r2.position.y + r2.height
 );
