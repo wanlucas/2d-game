a290e307c3b2340b23f973b0f0689e29cb7c53a5
Wan Lucas <wan.lucas.90@gmail.com>
2022-10-23 16:21:23 -0300

    :sparkles: adding Entity collision

--- src/class/Entity.js
+++ src/class/Entity.js
@@ -1,7 +1,21 @@
-import { ctx, canvasH } from '../game.js';
+import { ctx, canvasH, platforms } from '../game.js';
+import { rectanglesHorizontalCollision, rectanglesVerticalCollision } from '../functions/collision.js';
 
 export default class Entity {
   move() {
+    platforms.forEach((platform) => {
+      if (rectanglesHorizontalCollision(this, platform)) {
+        this.velocity.x = 0;
+      }
+
+      if (rectanglesVerticalCollision(this, platform)) {
+        if (this.position.y + this.height < platform.position.y) {
+          this.remainingJumps = 2;
+        }
+        this.velocity.y = 0;
+      }
+    });
+
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
   }
